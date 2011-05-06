fad33f17a149052541d99414f571a1432a365e1c
Evan Shaw <edsrzf@gmail.com>
2011-05-06 17:55:45 +1200

    Use Windows new syscalls and constants

--- mmap_windows.go
+++ mmap_windows.go
@@ -21,81 +21,31 @@ import (
 var handleLock sync.Mutex
 var handleMap = map[uintptr]int32{}
 
-var (
-	modkernel32 = loadDll("kernel32.dll")
-
-	procCreateFileMapping = getSysProcAddr(modkernel32, "CreateFileMappingW")
-	procMapViewOfFile     = getSysProcAddr(modkernel32, "MapViewOfFile")
-	procFlushViewOfFile   = getSysProcAddr(modkernel32, "FlushViewOfFile")
-	procUnmapViewOfFile   = getSysProcAddr(modkernel32, "UnmapViewOfFile")
-	procVirtualLock       = getSysProcAddr(modkernel32, "VirtualLock")
-	procVirtualUnlock     = getSysProcAddr(modkernel32, "VirtualUnlock")
-	procVirtualProtect    = getSysProcAddr(modkernel32, "VirtualProtect")
-)
-
-func loadDll(fname string) uint32 {
-	h, e := syscall.LoadLibrary(fname)
-	if e != 0 {
-		panic("LoadLibrary failed")
-	}
-	return h
-}
-
-func getSysProcAddr(m uint32, pname string) uintptr {
-	p, e := syscall.GetProcAddress(m, pname)
-	if e != 0 {
-		panic("GetProcAddress failed")
-	}
-	return uintptr(p)
-}
-
-const (
-	_PAGE_READONLY          = 0x02
-	_PAGE_READWRITE         = 0x04
-	_PAGE_WRITECOPY         = 0x08
-	_PAGE_EXECUTE_READ      = 0x20
-	_PAGE_EXECUTE_READWRITE = 0x40
-	_PAGE_EXECUTE_WRITECOPY = 0x80
-
-	_SEC_IMAGE        = 0x1000000
-	_SEC_RESERVE      = 0x4000000
-	_SEC_COMMIT       = 0x8000000
-	_SEC_NOCACHE      = 0x10000000
-	_SEC_WRITECOMBINE = 0x40000000
-	_SEC_LARGE_PAGES  = 0x80000000
-
-	_FILE_MAP_COPY       = 0x01
-	_FILE_MAP_WRITE      = 0x02
-	_FILE_MAP_READ       = 0x04
-	_FILE_MAP_ALL_ACCESS = 0x000F0000 | _FILE_MAP_COPY | _FILE_MAP_READ | _FILE_MAP_WRITE | 0x08 | 0x10
-	_FILE_MAP_EXECUTE    = 0x20
-)
-
 func mmap(len int, prot, flags, hfile uintptr, off int64) ([]byte, os.Error) {
-	flProtect := uintptr(_PAGE_READONLY)
-	dwDesiredAccess := uintptr(_FILE_MAP_READ)
+	flProtect := uint32(syscall.PAGE_READONLY)
+	dwDesiredAccess := uint32(syscall.FILE_MAP_READ)
 	switch {
 	case prot&COPY != 0:
-		flProtect = _PAGE_WRITECOPY
-		dwDesiredAccess = _FILE_MAP_COPY
+		flProtect = syscall.PAGE_WRITECOPY
+		dwDesiredAccess = syscall.FILE_MAP_COPY
 	case prot&RDWR != 0:
-		flProtect = _PAGE_READWRITE
-		dwDesiredAccess = _FILE_MAP_WRITE
+		flProtect = syscall.PAGE_READWRITE
+		dwDesiredAccess = syscall.FILE_MAP_WRITE
 	}
 	if prot&EXEC != 0 {
 		flProtect <<= 4
-		dwDesiredAccess |= _FILE_MAP_EXECUTE
+		dwDesiredAccess |= syscall.FILE_MAP_EXECUTE
 	}
 
 	// TODO: Do we need to set some security attributes? It might help portability.
-	h, _, errno := syscall.Syscall6(procCreateFileMapping, 6, uintptr(hfile), 0, flProtect, 0, uintptr(len), 0)
+	h, errno := syscall.CreateFileMapping(int32(hfile), nil, flProtect, 0, uint32(len), nil)
 	if h == 0 {
-		return nil, os.NewSyscallError("CreateFileMapping", int(errno))
+		return nil, os.NewSyscallError("CreateFileMapping", errno)
 	}
 
-	addr, _, errno := syscall.Syscall6(procMapViewOfFile, 5, h, dwDesiredAccess, uintptr(off>>32), uintptr(off&0xFFFFFFFF), uintptr(len), 0)
+	addr, errno := syscall.MapViewOfFile(h, dwDesiredAccess, uint32(off>>32), uint32(off&0xFFFFFFFF), uintptr(len))
 	if addr == 0 {
-		return nil, os.NewSyscallError("MapViewOfFile", int(errno))
+		return nil, os.NewSyscallError("MapViewOfFile", errno)
 	}
 	handleLock.Lock()
 	handleMap[addr] = int32(h)
@@ -113,25 +63,25 @@ func mmap(len int, prot, flags, hfile uintptr, off int64) ([]byte, os.Error) {
 }
 
 func flush(addr, len uintptr) os.Error {
-	_, _, errno := syscall.Syscall(procFlushViewOfFile, 2, addr, len, 0)
-	return os.NewSyscallError("FlushViewOfFile", int(errno))
+	errno := syscall.FlushViewOfFile(addr, len)
+	return os.NewSyscallError("FlushViewOfFile", errno)
 }
 
 func lock(addr, len uintptr) os.Error {
-	_, _, errno := syscall.Syscall(procVirtualLock, 2, addr, len, 0)
-	return os.NewSyscallError("VirtualLock", int(errno))
+	errno := syscall.VirtualLock(addr, len)
+	return os.NewSyscallError("VirtualLock", errno)
 }
 
 func unlock(addr, len uintptr) os.Error {
-	_, _, errno := syscall.Syscall(procVirtualUnlock, 2, addr, len, 0)
-	return os.NewSyscallError("VirtualUnlock", int(errno))
+	errno := syscall.VirtualUnlock(addr, len)
+	return os.NewSyscallError("VirtualUnlock", errno)
 }
 
 func unmap(addr, len uintptr) os.Error {
 	flush(addr, len)
-	r0, _, errno := syscall.Syscall(procUnmapViewOfFile, 1, addr, 0, 0)
-	if r0 == 0 {
-		return os.NewSyscallError("UnmapViewOfFile", int(errno))
+	errno := syscall.UnmapViewOfFile(addr)
+	if errno != 0 {
+		return os.NewSyscallError("UnmapViewOfFile", errno)
 	}
 
 	handleLock.Lock()
